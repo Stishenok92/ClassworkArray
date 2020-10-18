d79f7af04469ef3be4f03717596a6e0cb4276de7
Stishenok92 <Stishenok92@gmail.com>
2020-10-18 23:18:28 +0300

    add writeline in program

--- ArrayLibrary/Controller/Program.cs
+++ ArrayLibrary/Controller/Program.cs
@@ -13,16 +13,23 @@ namespace Program.Controller
             
             Console.WriteLine("Task 1\nFind mul of even numbers in array\n");
             int[] array1 = GetArrayInitRandom();
+            Console.WriteLine();
             Console.WriteLine(ShowArray(array1));
+            Console.WriteLine();
             Console.WriteLine("Multiplication: " + CalculateMulNumbersWithEvenIndex(array1));
-
+            Console.WriteLine();
+            
             Console.WriteLine("Task 2\nFind sum of array numbers located between first and last index with number zero\n");
             int[] array2 = GetArrayInitRandom();
+            Console.WriteLine();
             Console.WriteLine(ShowArray(array2));
+            Console.WriteLine();
             Console.WriteLine("Sum: " + CalculateSumBetweenFirstAndLastIndexWithNumberZero(array2));
-
+            Console.WriteLine();
+            
             Console.WriteLine("Task 3\nFind number of line containing longest series of identical numbers\n");
             int[][] matrix = GetMatrixInitRandom();
+            Console.WriteLine();
             Console.WriteLine(ShowMatrix(matrix));
             Console.WriteLine("Number line: " + FindIndexRowMaxSeriesNumber(matrix));
         }
